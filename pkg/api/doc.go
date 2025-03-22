2dd5a0204530deb9983bd4e8e1edfda81c99d0a1
Francesco Torta <62566275+fra98@users.noreply.github.com>
2025-03-22 20:43:25 +0100

    feat: handlers for API endpoints

---
+++ pkg/api/doc.go
@@ -0,0 +1,2 @@
+// Package api contains the handlers the API layer uses to interact with the service layer.
+package api
---
+++ pkg/api/handler.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/fra98/pokedex/pkg/server/httperror"
+	"github.com/fra98/pokedex/pkg/service"
+)
+
+// PokemonHandler handles the Pokemon API endpoints.
+type PokemonHandler struct {
+	pokemonService service.PokemonService
+}
+
+// NewPokemonHandler creates a new PokemonHandler with the given PokemonService.
+func NewPokemonHandler(pokemonService service.PokemonService) *PokemonHandler {
+	return &PokemonHandler{pokemonService: pokemonService}
+}
+
+// GetPokemon returns the information of a Pokemon given its name.
+func (h *PokemonHandler) GetPokemon(c *gin.Context) {
+	name := c.Param("name")
+
+	pokemon, err := h.pokemonService.GetPokemonInfo(name)
+	if err != nil {
+		err := httperror.NewHTTPError("unable to retrieve pokemon info", http.StatusNotFound)
+		_ = c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, pokemon)
+}
+
+// GetTranslatedPokemon returns the information of a Pokemon given its name with a translated description.
+func (h *PokemonHandler) GetTranslatedPokemon(c *gin.Context) {
+	name := c.Param("name")
+
+	pokemon, err := h.pokemonService.GetTranslatedPokemonInfo(name)
+	if err != nil {
+		err := httperror.NewHTTPError("unable to retrieve translated pokemon info", http.StatusNotFound)
+		_ = c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, pokemon)
+}
